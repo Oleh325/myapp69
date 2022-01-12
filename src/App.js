cb21d09305babec1ff222cbb33a15138d98aedc0
DESKTOP-POSS0RQ\olega <olegackiv956@gmail.com>
2022-01-12 21:14:50 +0200

    added New Post functionality

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
 import InputBasedTextBox from './components/InputBasedTextBox';
 import Post from './components/Post';
+import MyInput from './components/UI/Input/MyInput.jsx';
+import MyButton from './components/UI/Button/MyButton.jsx';
 import "./styles/App.css";
-import CreatePost from './components/CreatePost';
 
 function App() {
 	const [posts, setPosts] = useState([
-		{ id: "1", title: "C#", body: "C# is the best programming language" },
-		{ id: "2", title: "Java", body: "Java is also nice" },
-		{ id: "3", title: "???", body: "But do you know what is the best?" },
-		{ id: "0", title: "Nadiyka", body: "Yup" }
-	]);
+        { id: "1", title: "C#", body: "C# is the best programming language" },
+        { id: "2", title: "Java", body: "Java is also nice" },
+        { id: "3", title: "???", body: "But do you know what is the best?" },
+        { id: "0", title: "Nadiyka", body: "Yup" }
+    ]);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const addNewPost = (e) => {
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        e.preventDefault();
+        setPosts([...posts, newPost]);
+        console.log(newPost);
+    }
 	return (
 		<div className="App">
 			<ClassCounter />
 			<Counter />
 			<InputBasedTextBox />
-			<CreatePost />
+			<div>
+            <form>
+                {/*Controlled input*/}
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Name"
+                />
+                {/*Uncontrolled input*/}
+                <MyInput
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                    type="text"
+                    placeholder="Description"
+                />
+                <MyButton onClick={addNewPost}>Create</MyButton>
+            </form>
+        </div>
 			<h1 style={{ textAlign: 'center' }}>Posts</h1>
 			{posts.map(post =>
 				<Post post={post} key={post.id} />
